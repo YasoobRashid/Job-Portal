d437ddfa17e56a80773355ecfcad4a03e91756f3
Yasoob <yasoobjavaid080@gmail.com>
2025-12-23 19:02:36 +0530

    fix: enable static access to uploaded resume files for recruiters

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 
 
 const authRoutes = require("./routes/auth.routes");
@@ -13,6 +15,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
